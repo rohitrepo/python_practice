a553a721553ca1ddcd939caec1c0bade5e076331
Rohit Yadav <roityadav@gmail.com>
2024-04-13 19:35:45 +0530

    Adding after minor changes to diamond pattern

--- python_practice/basic_ques_practice/05_PrintDiamondPattern.py
+++ python_practice/basic_ques_practice/05_PrintDiamondPattern.py
@@ -1,14 +1,15 @@
 #printing diamond pattern using for loop
+# This example uses steps too
 
 print("Enter a number:")
 num = int ( input () )
 
-for i in range (1, num + 1):
+for i in range (1, num + 1, 2):
     if num % 2 == 0:
         print("Number is even")
         
     else:
-        (i + num)
+        start = 
         mid = (num %% 2) + 1
         
         print (" " * (mid - 1 ), "*", " " * (mid - 1 ))
